616927348ddd23a3ec448439bdf345591b0f7bf1
selman13 <selmaneliyev99@gmal.com>
2024-11-13 20:39:03 +0400

    Text component created

---
+++ components/common/Text.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Text = () => {
+  return (
+    <div className='max-w-[400px] w-full'>
+        Kitabsevərlər üçün yaradılmış və zəngin bir kolleksiya təklif edən
+      mağazamız, oxucuların istədikləri hər kitabı asanlıqla tapmalarına
+      kömək edir.
+    </div>
+  )
+}
+
+export default Text
