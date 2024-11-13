3a5f0f195015cda89a64a7bb5eb09440fe3bafb1
selman13 <selmaneliyev99@gmal.com>
2024-11-13 20:37:22 +0400

    banner data created

---
+++ MockData/Banner.ts
@@ -0,0 +1,22 @@
+export const BannerItems:IBannerItems[] = [
+    {
+        id:0,
+        item: "/banner1.svg",
+        type: "image"
+    },
+    {
+        id:1,
+        item: "/banner2.svg",
+        type: "image"
+    }, 
+    {
+        id:2,
+        item: "/banner3.svg",
+        type: "image"
+    },
+    {
+        id:3,
+        item: "/banner4.mp4",
+        type: "video"
+    }
+]
