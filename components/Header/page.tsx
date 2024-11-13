7a4965912ca05b8e0bffdf3c58f77f11d116fe62
selman13 <selmaneliyev99@gmal.com>
2024-11-13 20:42:05 +0400

    Header component edited

--- components/Header/page.tsx
+++ components/Header/page.tsx
@@ -5,19 +5,29 @@ import Link from 'next/link'
 import { MdFavorite } from 'react-icons/md'
 import MobileNav from '../common/MobileNav'
 import Basket from '../common/Basket'
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+
 
 const Header = () => {
   return (
     <div className='flex justify-between items-center gap-3 md:gap-10 p-5 bg-red-800 text-white'>
         <div>
-            <Logo/>
+           <Link href="/"> <Logo/></Link>
         </div>
             <div className='hidden md:flex flex-1 justify-center items-center gap-5'>
                <Search/>
                 <Link href="/books">Kitablar</Link>
                 <Link href="/basket"><Basket/></Link>
                 <Link href="/favorite"><MdFavorite size={25}/></Link>
-                <Link href="/contact">Giriş</Link>
+                <Link href="/contact">Əlaqə</Link>
+                <SignedOut>
+            <SignInButton>
+            <Link href="/login">Giriş</Link>
+            </SignInButton>
+          </SignedOut>
+          <SignedIn>
+            <UserButton/>
+          </SignedIn>
             </div>
             <div className='flex md:hidden'>
                 <MobileNav/>
