343a684b8df968d70e903f4a54fd443708e7879e
selman13 <selmaneliyev99@gmal.com>
2024-11-13 20:38:36 +0400

    Slogan component created

---
+++ components/common/Slogan.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Slogan = () => {
+  return (
+    <div>
+        <h1 className='tracking-widest'>Azərbaycanın kitab SFERAsına xoş gəlmisiniz</h1>
+    </div>
+  )
+}
+
+export default Slogan
