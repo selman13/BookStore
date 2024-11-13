521180867df0be08328a6eca964df662e5007f53
selman13 <selmaneliyev99@gmal.com>
2024-11-13 20:41:46 +0400

    Footer component edited

--- components/Footer/page.tsx
+++ components/Footer/page.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import Logo from "../common/Logo";
+import Text from "../common/Text";
+import SocialMedia from "../common/SocialMedia";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="bg-red-800 text-white flex flex-col md:flex-row justify-between items-start md:items-center p-5">
+      <div className="flex flex-col gap-3">
+        <Logo />
+        <Text />
+      </div>
+      <SocialMedia/>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
