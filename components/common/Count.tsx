8d1efcc401277967a34ce8ef9c44e695f77d0802
selman13 <selmaneliyev99@gmal.com>
2024-11-13 15:02:14 +0400

    Count component created

---
+++ components/common/Count.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Count = () => {
+  return (
+    <div className='absolute -top-3 left-5 rounded-full w-5 h-5 bg-white text-red-900 flex justify-center items-center'>0</div>
+  )
+}
+
+export default Count
