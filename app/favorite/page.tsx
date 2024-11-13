4a71cf52b7b816d613624c2a8644771802b7310b
selman13 <selmaneliyev99@gmal.com>
2024-11-13 15:01:45 +0400

    Favorite page created

---
+++ app/favorite/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { MdFavorite } from 'react-icons/md'
+
+const Favorite = () => {
+  return (
+    <div>
+        <MdFavorite size={25}/>
+    </div>
+  )
+}
+
+export default Favorite
